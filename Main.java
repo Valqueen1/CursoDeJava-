66ad39f0a5fac171ae7aec89537c81826ba408e8
VaL <91805686+Valqueen1@users.noreply.github.com>
2022-08-25 01:27:34 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        float num1=5;
+        float num2=2;
+        float num3=3;
+        float result, numerador, denom;
+        float mult1, mult2, mult3;
+
+        mult1=(num2/num3);
+        mult2=(7/num2);
+        mult3=mult1*mult2;
+        numerador=(6/num1)-(mult3)+(num2/30);
+        denom= (1/num3)/5;
+        rdo=numerador/denom;
+        System.out.println(+result);
+    }
+}
